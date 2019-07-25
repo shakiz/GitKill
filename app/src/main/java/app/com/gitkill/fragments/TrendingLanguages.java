c92def1e333c92991d4a17b32f9921ddff57b674
shakiz <activities335@gmail.com>
2019-07-25 19:51:14 +0600

    Added the basic functionalities and design for fragments.

--- app/src/main/java/app/com/gitkill/fragments/TrendingLanguages.java
+++ app/src/main/java/app/com/gitkill/fragments/TrendingLanguages.java
@@ -1,16 +1,31 @@
 package app.com.gitkill.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import app.com.gitkill.R;
 
 public class TrendingLanguages extends Fragment {
 
+    private ArrayAdapter<String> arrayAdapter;
+    private Spinner timeSpinner;
+    private Context context;
+    private ArrayList<String>  timeList;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
 
     public TrendingLanguages() {
     }
@@ -20,7 +35,29 @@ public class TrendingLanguages extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_trending_languages, container, false);
+        init(view);
+        setData();
+        setAdapter(timeSpinner,timeList);
         return view;
+    }
+
+    private void setData() {
+        //Adding data to time list
+        timeList.add("Daily");
+        timeList.add("Weekly");
+        timeList.add("Monthly");
+        timeList.add("Yearly");
+    }
+
+    private void setAdapter(Spinner spinner, ArrayList<String> languageList) {
+        arrayAdapter = new ArrayAdapter<>(context,R.layout.spinner_drop,languageList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+    }
+
+    private void init(View view) {
+        timeSpinner = view.findViewById(R.id.TimeSpinner);
+        timeList = new ArrayList<>();
     }
 
 }
--- app/src/main/java/app/com/gitkill/fragments/TrendingUsers.java
+++ app/src/main/java/app/com/gitkill/fragments/TrendingUsers.java
@@ -1,16 +1,32 @@
 package app.com.gitkill.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import app.com.gitkill.R;
 
 public class TrendingUsers extends Fragment {
 
+    private ArrayAdapter<String> arrayAdapter;
+    private Spinner languageSpinner,timeSpinner;
+    private Context context;
+    private ArrayList<String> languageList, timeList;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
 
     public TrendingUsers() {
     }
@@ -20,7 +36,36 @@ public class TrendingUsers extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_trending_users, container, false);
+        init(view);
+        setData();
+        setAdapter(languageSpinner,languageList);
+        setAdapter(timeSpinner,timeList);
         return view;
     }
 
+    private void setData() {
+        //Adding the language list
+        languageList.add("Java");
+        languageList.add("Python");
+        languageList.add("Javascript");
+        languageList.add("PHP");
+        //Adding data to time list
+        timeList.add("Daily");
+        timeList.add("Weekly");
+        timeList.add("Monthly");
+        timeList.add("Yearly");
+    }
+
+    private void setAdapter(Spinner spinner, ArrayList<String> languageList) {
+        arrayAdapter = new ArrayAdapter<>(context,R.layout.spinner_drop,languageList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+    }
+
+    private void init(View view) {
+        languageSpinner = view.findViewById(R.id.LanguageSpinner);
+        timeSpinner = view.findViewById(R.id.TimeSpinner);
+        languageList = new ArrayList<>();
+        timeList = new ArrayList<>();
+    }
 }
